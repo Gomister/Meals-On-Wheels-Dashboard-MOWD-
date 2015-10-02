7607f86738c3b68a62152c734edabc27ff85c463
Giovanni Eugene <geugene1@fau.edu>
2015-10-02 20:55:04 +0000

    MOWD

--- index.php
+++ index.php
@@ -11,11 +11,9 @@
 			<td style='width: 30%;'><img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello world!"; ?>
+				<h1 id = "message"><?php echo "Welcome to Meals-On-Wheels Dashboard"; ?>
 </h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>. Get started by reading our <a
-				href="https://www.ng.bluemix.net/docs/#starters/php/index.html#php">documentation</a>
-				or use the Start Coding guide under your app in your dashboard.
+				<p class='description'></p> Coming Soon <span class="blue">Stay tuned</span>
 			</td>
 		</tr>
 	</table>
