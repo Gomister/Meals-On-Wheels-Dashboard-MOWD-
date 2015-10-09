cbbe28bf6f27c49cc2a90f77db12821cb293c230
Giovanni Eugene <geugene1@fau.edu>
2015-10-09 13:05:29 -0400

    Made a lot of changes.

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
+	<title>Meals On Wheels Dashboard</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"  />
+	
 </head>
 <body>
-	<table>
-		<tr>
-			<td>
-				<h1 id = "message"><?php echo "Welcome to Meals-On-Wheels Dashboard"; ?>
-</h1>
-				<p class='description'></p> Coming Soon <span class="blue">Stay tuned</span>
-			</td>
-		</tr>
-	</table>
+	<div class="container-fluid">
+	<!-- Start Header	-->
+    <div class="header">
+        <h1> Welcome to the Meals-On-Wheels Dashboard! </h1>
+    </div>
+	<!-- End Header		-->
+    
+    <div class="row">
+    	<h2>Are you a...</h2>
+    </div>
+        
+	<div class="row-centered">
+   		<div class="col-xs-12 col-md-3 col-md-offset-1"><a class="btn btn-primary btn-lg" href="#" role="button">Volunteer</a></div> 
+    	<div class="col-xs-12 col-md-4 "><h2>Or...</h2></div> 
+        <div class="col-xs-12 col-md-3 "><a class="btn btn-primary btn-lg" href="#" role="button">Administrator</a></div> 
+    </div>
+	
+	
+	
+	</div>
+	
+	
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>	
 </body>
 </html>
