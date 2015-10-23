3871d58b61e64c6cbc350c0a2210f1941f8f1238
Giovanni Eugene <geugene1@fau.edu>
2015-10-23 13:19:05 -0400

    A change was made

---
+++ Volunteer/Login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Volunteer Login - Meals On Wheels Dashboard</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="../css/style.css" rel="stylesheet"  />
+	
+</head>
+<body>
+	<!-- Start Header	-->
+    <div class="header">
+        <h1> Volunteer Login </h1>
+    </div>
+	<!-- End Header		-->
+    <div class="container">
+        <form>
+            <div class="row-centered">
+                <div class="col-xs-12 col-md-6 col-centered">
+                    <div class="form-group">
+                        <label for=volunteerUsername> <h2>Username</h2> </label>
+                        <input type="text" class="form-control" id="volunteerUsername" placeholder="Username">
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-md-6 col-centered">
+                    <div class="form-group">
+                        <label for=volunteerPassword> <h2>Password</h2> </label>
+                        <input type="password" class="form-control" id="volunteerPassword" placeholder="Password">
+                    </div>
+                </div>
+            </div>
+            <div class="row-centered">
+                <button type="submit" class="btn btn-primary btn-lg row-centered btn-block">Submit</button>
+            </div>
+        </form>
+	
+        <hr>
+        
+        <h2>Not a registered volunteer? <a href="#">Register here!</a></h2>
+	
+        
+        
+        
+    </div>
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>	
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,31 +3,27 @@
 <head>
 	<title>Meals On Wheels Dashboard</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.min.css" rel="stylesheet"/>
     <link href="css/style.css" rel="stylesheet"  />
 	
 </head>
 <body>
-	<div class="container-fluid">
 	<!-- Start Header	-->
     <div class="header">
         <h1> Welcome to the Meals-On-Wheels Dashboard! </h1>
     </div>
 	<!-- End Header		-->
-    
-    <div class="row">
-    	<h2>Are you a...</h2>
-    </div>
+    <div class="container">
+        <div class="row">
+            <h2>Are you a...</h2>
+        </div>
         
-	<div class="row-centered">
-   		<div class="col-xs-12 col-md-3 col-md-offset-1"><a class="btn btn-primary btn-lg" href="#" role="button">Volunteer</a></div> 
-    	<div class="col-xs-12 col-md-4 "><h2>Or...</h2></div> 
-        <div class="col-xs-12 col-md-3 "><a class="btn btn-primary btn-lg" href="#" role="button">Administrator</a></div> 
-    </div>
-	
-	
-	
+        <div class="row-centered">
+            <div class="col-xs-12 col-md-4 col-centered"><h2><a class="btn btn-primary btn-lg" href="Volunteer/Login.php" role="button">Volunteer</a></h2></div> 
+            <div class="col-xs-12 col-md-4 col-centered"><h2>Or...</h2></div> 
+            <div class="col-xs-12 col-md-4 col-centered"><h2><a class="btn btn-primary btn-lg" href="#" role="button">Administrator</a></h2></div> 
+        </div>
 	</div>
 	
 	
