15620dcd415f123c9c610d0753062bc338ace605
Giovanni Eugene <geugene1@fau.edu>
2015-12-01 14:00:56 -0500

    My web developer made some changes.

--- startEverything.php
+++ startEverything.php
@@ -6,15 +6,12 @@
         <head>";
     include_once 'databaseConnect.php';
     
-    if (isset($_SESSION['user']))
     if (isset($_SESSION['userID']))
     {
         $userName = $_SESSION['userName'];
         $userID = $_SESSION['userID'];
         $loggedIn = TRUE;
     }
-    else $loggedIn = FALSE;
-    
     else 
     {
     	$loggedIn = FALSE;
@@ -24,6 +21,5 @@
         <meta http-equiv='Content-Type content='text/html, charset=utf-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <link href='../css/bootstrap.min.css' rel='stylesheet'/>
-        <link href='../css/style.css' rel='stylesheet'  />
          ";
 ?>
