40534b09460cd59b4a7f815ab334073acf4fec84
Giovanni Eugene <geugene1@fau.edu>
2016-01-06 15:26:37 -0500

    A change here and there.

--- startEverything.php
+++ startEverything.php
@@ -20,6 +20,6 @@
     echo "
         <meta http-equiv='Content-Type content='text/html, charset=utf-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link href='../css/bootstrap.min.css' rel='stylesheet'/>
+        <link href='css/bootstrap.min.css' rel='stylesheet'/>
          ";
 ?>
